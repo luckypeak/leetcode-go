dc26c6779a5170313864113a9668c065dbac7daa
luckypeak <luckypeak>
2020-11-22 01:52:10 +0800

    add bfs

--- 129_root_leaf_sum.go
+++ 129_root_leaf_sum.go
@@ -2,15 +2,47 @@ package leetcode_go
 
 
 var ans = 0
+/**
+  使用dfs 遍历根到每个叶子节点的路径，并记录过程中的路径。到达叶子节点加和
+ */
 func sumNumbers(root *TreeNode) int {
 	ans = 0
 	if root == nil{
 		return 0
 	}
-	dfs(root, 0)
-	return ans
+
+	return bfs(root)
 }
 
+// 使用bfs，两个队列，一个队列放节点，一个队列放数据
+func bfs(node *TreeNode) int {
+	queue := make([]*TreeNode, 0)
+	numQueue := make([]int , 0)
+	queue = append(queue, node)
+	sum := 0
+	num := 0
+	numQueue = append(numQueue, node.Val)
+	for len(queue) > 0{
+		node = queue[0]
+		queue = queue[1:]
+		num = numQueue[0]
+		numQueue = numQueue[1:]
+		if node.Left == nil && node.Right == nil{
+			sum += num
+		}else{
+			if node.Left != nil{
+				queue = append(queue, node.Left)
+				numQueue = append(numQueue, num * 10 + node.Left.Val)
+			}
+			if node.Right != nil{
+				queue = append(queue, node.Right)
+				numQueue = append(numQueue, num * 10 + node.Right.Val)
+			}
+		}
+	}
+
+	return sum
+}
 
 func dfs(node *TreeNode, sum int) int {
 	if node == nil{
